0e003ffd8e938446f2d4e92d0dcaf20f07bff56d
Alan Gutierrez <alan@prettyrobots.com>
2013-08-04 18:56:23 -0400

    Generate a function.
    Closes #7.

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-module.exports = 1
+var __slice = [].slice
+
+function Source () {
+    this._snippets = []
+}
+
+var $ = /^((?:[^,\\'"\s\/()$]|\\.|(["'])(?:[^\\\1]|\\.)*\2)*)(.*)/
+
+Source.prototype.push = function (block) {
+    var source = String(block).split('\n')
+    var parameters = /^function \(([^)]*)\) {$/.exec(source[0])[1].trim()
+    parameters = parameters ? parameters.split(/, /) : []
+    source.shift()
+    source.pop()
+    console.log(parameters, source)
+    var spaces = Number.MAX_VALUE
+    source.forEach(function (line) {
+        if (/\S/.test(line)) {
+            spaces = Math.min(spaces, line.length - line.replace(/^\s+/, '').length)
+        }
+    })
+    source = source.map(function (line) {
+        if (/\S/.test(line)) {
+            return line.substring(spaces)
+        } else {
+            return ''
+        }
+    })
+    source.forEach(function (line) {
+        this._snippets.push([ line ])
+    }, this)
+}
+
+function indent (source) {
+    return source.map(function (line) {
+        return '    ' + line
+    })
+}
+
+Source.prototype.compiler = function () {
+    return function () {
+        var parameters = __slice.call(arguments)
+        var source = []
+        this._snippets.forEach(function (snippet) {
+            source.push(snippet.join(''))
+        })
+        source = indent(source)
+        return Function.apply(Function, parameters.concat(source.join('\n')))
+    }.bind(this)
+}
+
+function createSource () {
+    var source = new Source
+    return function () {
+        var vargs = __slice.call(arguments)
+        if (vargs.length) {
+            source.push(vargs.shift())
+        } else {
+            return source.compiler()
+        }
+    }
+}
+
+module.exports.createSource = createSource
