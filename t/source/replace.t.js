e3895216f3fdcf17f9494b22e061ccfa82933ebf
Alan Gutierrez <alan@prettyrobots.com>
2013-08-04 22:35:28 -0400

    Add tests for existing rewrite.
    See #8.

--- t/source/replace.t.js
+++ t/source/replace.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('proof')(1, function (equal) {
+require('proof')(2, function (equal) {
     var source = require('../..').createSource()
 
     source(function () {
@@ -12,5 +12,20 @@ require('proof')(1, function (equal) {
 
     console.log(String(method))
 
-    equal(method(), 1, 'generated')
+    equal(method(), 1, 'replace with string')
+
+    source('number', function () {
+        1
+    })
+    method = source()()
+    console.log(String(method))
+    equal(method(), 1, 'replace with source')
+
+    source('number', function () {
+        2 -
+        1
+    })
+    method = source()()
+    console.log(String(method))
+    equal(method(), 1, 'replace with multi-line source')
 })
