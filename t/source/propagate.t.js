33366156808bd4e9e9faa44a13a0873ef14d66b9
Alan Gutierrez <alan@prettyrobots.com>
2013-08-07 00:03:59 -0400

    Can't always test `toString` due to minification.
    See #27.

--- t/source/propagate.t.js
+++ t/source/propagate.t.js
@@ -1,4 +1,4 @@
-require('proof')(2, function () {
+require('proof')(1, function () {
     var source = require('../..')
 
     var block = source(function () {
@@ -8,7 +8,6 @@ require('proof')(2, function () {
     block.$multiple(function () { 2 })
 
     console.log(String(block))
-    this.equal(String(block), 'return 2 * number\n', 'convert to string')
 
     var double = block.compile('number')
     this.ok(double(2) == 4, 'generated function')
