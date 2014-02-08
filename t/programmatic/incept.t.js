b2ba7af7fc2e964dc25da03eac5c4fe38560bb7e
Alan Gutierrez <alan@prettyrobots.com>
2014-02-08 14:34:44 -0800

    AST of AST converter with identifier keys.
    Fix the AST to AST of AST converter to generate an AST where the
    proeprty keys are identifiers and not string literals.

--- t/programmatic/incept.t.js
+++ t/programmatic/incept.t.js
@@ -1,4 +1,9 @@
 require('../proof')(1, function (dump, deepEqual) {
     var incept = require('../../lib/incept')
-    deepEqual(incept({}), { type: 'ObjectExpression', properties: [] }, 'object')
+    deepEqual(incept({ a: 1 }), { type: 'ObjectExpression', properties: [
+        { type: 'Property',
+          key: { type: 'Identifier', name: 'a' },
+          value: { type: 'Literal', value: 1 },
+          kind: 'init' }
+    ] }, 'object')
 })
