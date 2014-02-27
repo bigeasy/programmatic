3bd90f3a3d0ba8adba042ef2d8c983b613a04814
Alan Gutierrez <alan@prettyrobots.com>
2014-02-27 11:00:50 -0800

    Tidy `redux.t.js`, removing first draft.
    See #60.

--- t/programmatic/redux.t.js
+++ t/programmatic/redux.t.js
@@ -1,22 +1,15 @@
-require('proof')(1, function (deepEqual) {
+var loop = 'var i = 0\n\
+while (i < 10) {\n\
+    i++\n\
+}'
+
+require('proof')(1, function (equal) {
     var redux  = require('../../redux')
-    deepEqual(redux.redent(1, '\
-        while (true) {                  \n\
-            index++                     \n\
-            if (index == 10) break      \n\
-        }'), [
-        [ 1, 'while (true) {' ],
-        [ 2, 'index++' ],
-        [ 2, 'if (index == 10) break' ],
-        [ 1, '}' ]
-    ], 'dedent')
-
-    var s = redux.concept, o = redux.concept, i = 0
-
-    console.log(s('                     \n\
+    var s = redux.concept
+    equal(s('                     \n\
         var i = 0                       \n\
         while (i < 10) {                \n\
-            ', o('i++'), '              \n\
+            ', s('i++'), '              \n\
         }                               \n\
-    '))
+    '), loop, 'indented')
 })
