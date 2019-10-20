3e62592c4080f9664d9ac90edb2f66b165adb7aa
Alan Gutierrez <alan@prettyrobots.com>
2019-10-20 13:15:01 -0500

    Use template strings in tests.
    Closes #102.

--- test/programmatic.t.js
+++ test/programmatic.t.js
@@ -1,67 +1,67 @@
-var loop = 'var i = 0\n\
-while (i < 10) {\n\
-    i++ // foo\n\
-    console.log(i)\n\
-}'
+var loop = `var i = 0
+while (i < 10) {
+    i++ // foo
+    console.log(i)
+}`
 
-var f = 'function () {\n\
-    var i = 0\n\
-    while (i < 10) {\n\
-        i++ // foo\n\
-        console.log(i)\n\
-    }\n\
-    console.log(i)\n\
-}'
+const f = `function () {
+    var i = 0
+    while (i < 10) {
+        i++ // foo
+        console.log(i)
+    }
+    console.log(i)
+}`
 
-var blank = 'function () {\n\
-    var i = 0\n\
-    while (i < 10) {\n\
-        i++ // foo\n\
-        console.log(i)\n\
-    }\n\
-\n\
-    console.log(i)\n\
-}'
+var blank = `function () {
+    var i = 0
+    while (i < 10) {
+        i++ // foo
+        console.log(i)
+    }
+
+    console.log(i)
+}`
 
 require('proof')(5, prove)
 
 function prove (assert) {
     var s  = require('..'), inner
-    assert(inner = s('                  \n\
-        var i = 0                       \n\
-        while (i < 10) {                \n\
-            ', '', '                    \n\
-            ', 'i++', ' // foo          \n\
-            console.log(i)              \n\
-        }                               \n\
-    '), loop, 'indented')
-    assert(s(['                         \n\
-        function () {                   \n\
-            ', inner, '                 \n\
-                                        \n\
-            console.log(i)              \n\
-            // __reference__            \n\
-        }                               \n\
-    ']), f, 'nested multi-line')
-    assert(s(['                         \n\
-        function () {                   \n\
-            ', inner, '                 \n\
-            // __blank__                \n\
-            console.log(i)              \n\
-        }                               \n\
-    ']), blank, 'nested blank line')
-    assert(s('                          \n\
-        var i = 0                       \n\
-        while (i < 10) {                \n\
-            i++ // foo                  \n\
-            console.log(i)              \n\
-        ', '}', '                       \n\
-    '), loop, 'dedented')
-    assert(s('                          \n\
-        var i = 0                       \n\
-        ', 'while (i < 10)', ' {        \n\
-            i++ // foo                  \n\
-            console.log(i)              \n\
-        }                               \n\
-    '), loop, 'unchanged')
+    assert(inner = s(`
+        var i = 0
+        while (i < 10) {
+            `, '', `
+            `, 'i++', ` // foo
+            console.log(i)
+        }
+    `), loop, 'indented')
+    assert(s([`
+        function () {
+            `, inner, `
+
+            console.log(i)
+            // __reference__
+        }
+    `]), f, 'nested multi-line')
+    assert(s([`
+        function () {
+            `, inner, `
+            // __blank__
+            console.log(i)
+        }
+    `]), blank, 'nested blank line')
+    assert(s(`
+        var i = 0
+        while (i < 10) {
+            i++ // foo
+            console.log(i)
+        `, '}', `
+    `), loop, 'dedented')
+    assert(s(`
+        var i = 0
+        `, 'while (i < 10)', ` {
+            i++ // foo
+            console.log(i)
+        }
+    `), loop, 'unchanged')
 }
